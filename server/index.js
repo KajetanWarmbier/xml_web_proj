09830d17cd451007102d0205fbbe30117468f3b0
Kajetan Warmbier <kajetan.warmbier@gmail.com>
2022-01-14 23:30:06 +0100

    xml_web_proj initial commit

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+
+const index = express();
+
+index.use(express.json());
+application.use(cors());
+
+module.exports = index;
---
+++ server/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const index = require("./index");
+
+const port = process.env.port || 5000;
+
+const server = http.createServer(index);
+
+server.listen(port);
